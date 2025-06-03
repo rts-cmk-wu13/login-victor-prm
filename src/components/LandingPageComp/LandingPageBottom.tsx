a27c6d2b2feeb9ca88f030c09ae04591da8280e4
victor-prm <victorpabb@gmail.com>
2025-06-03 16:06:54 +0200

    finished Landing Page design

--- src/components/LandingPageComp/LandingPageBottom.tsx
+++ src/components/LandingPageComp/LandingPageBottom.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router"
+import { ArrowLongRightIcon } from "@heroicons/react/16/solid";
 
 export default function LandingPageBottom() {
-    const baseButtonClass = "shadow-1 font-bold text-xl text-center p-4 rounded-[24px] w-full ";
+    const baseButtonClass = "shadow-1 font-bold text-xl text-center p-4 rounded-[24px] w-full inline-flex align-center justify-center gap-2";
 
     return (
         <div className="flex flex-col gap-2">
             <Link className={`${baseButtonClass} text-white bg-primary`} to="/signup">Sign up</Link>
-            <Link className={`${baseButtonClass} text-primary`} to="/login">Log in</Link>
+            <Link className={`${baseButtonClass} text-primary`} to="/login">Login <ArrowLongRightIcon className="size-5 h-auto"/></Link>
         </div>
     )
 }
